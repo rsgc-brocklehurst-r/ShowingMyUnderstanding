c0e65ff0d18935d0d3491be2533a1772ef11effe
Student <Ryan@macbook-pro-0403-ryan-brocklehurst.local>
2017-02-27 09:55:50 -0500

    made circle that is randomly filled or not filled

--- ShowingMyUnderstanding/ShowingMyUnderstanding.playground/Pages/Conditionals - Create an Example.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding/ShowingMyUnderstanding.playground/Pages/Conditionals - Create an Example.xcplaygroundpage/Contents.swift
@@ -21,6 +21,17 @@ let canvas = Canvas(width: 300, height: 300)
 
 // Replace this command and add your code below...
 
+
+let x =  random(from: 0, toButNotIncluding: 2)
+    if x == 0 {
+        canvas.drawShapesWithFill = false
+    }
+    if x == 1 {
+        canvas.drawShapesWithFill = true
+    }
+
+canvas.drawEllipse(centreX: 150, centreY: 150, width: 100, height: 100)
+
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
